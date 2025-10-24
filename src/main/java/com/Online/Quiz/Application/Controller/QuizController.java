c49c36a523edfd79c2ceb886a880567a63dd251b
USER <mayankrohan998@gmail.com>
2025-10-24 20:27:53 +0530

    quiz created

---
+++ src/main/java/com/Online/Quiz/Application/Controller/QuizController.java
@@ -0,0 +1,57 @@
+package com.Online.Quiz.Application.Controller;
+
+import com.Online.Quiz.Application.DTO.QuizDto;
+import com.Online.Quiz.Application.Service.QuizService;
+import com.Online.Quiz.Application.entity.Question;
+import com.Online.Quiz.Application.entity.Quiz;
+import com.Online.Quiz.Application.repository.QuizRepository;
+import org.aspectj.weaver.ResolvedPointcutDefinition;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/quizzes")
+public class QuizController {
+    private QuizService quizService;
+    private QuizRepository quizRepository;
+
+    public QuizController(QuizService quizService, QuizRepository quizRepository) {
+        this.quizService = quizService;
+        this.quizRepository = quizRepository;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createQuiz(@RequestBody Quiz quiz){
+        if (quiz.getQuestions() != null) {
+            for (Question question : quiz.getQuestions()) {
+                question.setQuiz(quiz);
+            }
+        }
+        Quiz savedQuiz = quizRepository.save(quiz);
+        return ResponseEntity.ok(savedQuiz);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Quiz>> getAllQuizzes(){
+        return quizService.getAllQuiz();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getQuizById(@PathVariable Long id){
+        return quizService.getQuizById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateQuiz(@PathVariable Long id, @RequestBody Quiz quiz){
+        return quizService.updateQuiz(id, quiz);
+    }
+
+    @DeleteMapping("/delete/{id}")
+
+    public ResponseEntity<?> deleteQuiz(@PathVariable Long id){
+        quizService.deleteQuiz(id);
+        return new ResponseEntity<>("Quiz deleted successfully", HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/Online/Quiz/Application/Service/QuizService.java
@@ -0,0 +1,66 @@
+package com.Online.Quiz.Application.Service;
+
+import com.Online.Quiz.Application.DTO.QuestionDto;
+import com.Online.Quiz.Application.DTO.QuizDto;
+import com.Online.Quiz.Application.entity.Question;
+import com.Online.Quiz.Application.entity.Quiz;
+import com.Online.Quiz.Application.repository.QuestionRepository;
+import com.Online.Quiz.Application.repository.QuizRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class QuizService {
+
+    private QuizRepository quizRepository;
+    private QuestionRepository questionRepository;
+
+    // Create a new quiz
+    public QuizService(QuizRepository quizRepository, QuestionRepository questionRepository) {
+        this.quizRepository = quizRepository;
+        this.questionRepository = questionRepository;
+    }
+    public ResponseEntity<String> createQuize(Quiz quiz){
+        if (quiz.getQuestions() != null) {
+            quiz.getQuestions().forEach(q -> q.setQuiz(quiz));
+        }
+        quizRepository.save(quiz);
+        return ResponseEntity.ok("✅ Quiz created and saved in database!");
+            }
+
+    // Get all quizzes
+    public ResponseEntity<List<Quiz>> getAllQuiz(){
+        return ResponseEntity.ok(quizRepository.findAll());
+    }
+
+    // Get quiz by id
+    public ResponseEntity<?> getQuizById(Long id) {
+        Optional<Quiz> quiz = quizRepository.findById(id);
+        if (quiz.isPresent()) {
+            return ResponseEntity.ok(quiz.get());
+        }
+        return ResponseEntity.badRequest().body("Quiz not found!");
+    }
+    // Update quiz
+    public ResponseEntity<?> updateQuiz(Long id, Quiz updatedQuiz){
+        Optional<Quiz> existingQuiz = quizRepository.findById(id);
+        if(existingQuiz.isPresent()){
+            Quiz quiz = existingQuiz.get();
+            quiz.setTitle(updatedQuiz.getTitle());
+            quizRepository.save(quiz);
+            return ResponseEntity.ok("Quiz updated successfully!");
+        }
+        return ResponseEntity.badRequest().body("Quiz not found");
+    }
+
+    // Delete quiz
+    public void deleteQuiz(Long id){
+        quizRepository.deleteById(id);
+//        /return ResponseEntity.badRequest().body("Quiz not found!");
+    }
+
+}
